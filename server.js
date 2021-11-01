7f7181b03e41230b074ccf4a4ebd2c41014c4d56
mostafaelhabrok <70176106+mostafaelhabrok@users.noreply.github.com>
2021-11-01 21:50:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.static('website'));
 // mysql
 var mysql = require('mysql');
 
+/*
 var con = mysql.createConnection({
   host: "remotemysql.com",
   user: "oInlIDhHL4",
@@ -31,6 +32,39 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+*/
+//connect
+var db_config = {
+  host: "remotemysql.com",
+  user: "oInlIDhHL4",
+  password: "gfSWeNRSXp",
+  database: "oInlIDhHL4"
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 // Setup Server
 
